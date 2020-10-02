ebc71a88ae67b6b3703fa3c12c6c8317497df437
obulareddyveera <obulareddy.veera@gmail.com>
2020-10-02 19:27:04 +0530

    updated baseURL for OAuth2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const port = 3000;
-const baseURL = `http://localhost:${port}`;
+const baseURL = `http://api.farmer.accounts.easyfarm.co.in/:${port}`;
 module.exports = {
   // The secret for the encryption of the jsonwebtoken
   JWTsecret: "mysecret",
