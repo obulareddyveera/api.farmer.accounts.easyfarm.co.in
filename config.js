1a5780d2f79bc4b2213944858fc77f136458252b
obulareddyveera <obulareddy.veera@gmail.com>
2020-10-02 18:32:24 +0530

    Configured Google LoginIn API

---
+++ config.js
@@ -0,0 +1,25 @@
+const port = 3000;
+const baseURL = `http://localhost:${port}`;
+module.exports = {
+  // The secret for the encryption of the jsonwebtoken
+  JWTsecret: "mysecret",
+  baseURL: baseURL,
+  port: port,
+  // The credentials and information for OAuth2
+  oauth2Credentials: {
+    client_id:
+      "643540525219-947emudk7cr2svpavlc621epqiie02fs.apps.googleusercontent.com",
+    project_id: "easyfarm-291212", // The name of your project
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_secret: "-Zz2R5yOnOyZjgQyyr5Qewbl",
+    redirect_uris: [`${baseURL}/auth_callback`],
+    scopes: [
+      "https://mail.google.com/",
+      'https://www.googleapis.com/auth/calendar',
+      'https://www.googleapis.com/auth/contacts',
+      'https://www.googleapis.com/auth/userinfo.profile',
+    ],
+  },
+};
---
+++ routes/oAuth.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const https = require("https");
+const router = express.Router();
+const google = require("googleapis").google;
+const jwt = require("jsonwebtoken");
+const CONFIG = require("./../config");
+
+router.get("/", (req, res) => {
+  const oauth2Client = new google.auth.OAuth2(
+    CONFIG.oauth2Credentials.client_id,
+    CONFIG.oauth2Credentials.client_secret,
+    CONFIG.oauth2Credentials.redirect_uris[0]
+  );
+  const loginLink = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: CONFIG.oauth2Credentials.scopes,
+  });
+
+  return res.json({ loginLink: loginLink });
+});
+
+router.get("/auth_callback", async (req, res) => {
+  const oauth2Client = new google.auth.OAuth2(
+    CONFIG.oauth2Credentials.client_id,
+    CONFIG.oauth2Credentials.client_secret,
+    CONFIG.oauth2Credentials.redirect_uris[0]
+  );
+  const { query } = req;
+  if (query.error) {
+    return res.json({ error: query.error });
+  } else {
+    const { err, tokens } = await oauth2Client.getToken(query.code);
+    if (err) return res.json({ error: err });
+    const { access_token } = tokens;
+    const googleProfileUrl = `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`;
+    https
+      .get(googleProfileUrl, (resp) => {
+        let data = "";
+        resp.on("data", (chunk) => {
+          data += chunk;
+        });
+        resp.on("end", () => {
+          console.log(JSON.parse(data));
+
+          return res.json({
+            tokens,
+            profile: JSON.parse(data),
+            jwt: jwt.sign(tokens, CONFIG.JWTsecret),
+          });
+        });
+      })
+      .on("error", (err) => {
+        return res.json({ error: err });
+      });
+  }
+});
+
+module.exports = router;
