c8f4e096406c6ba6cdd39042ad245b03c10cc6fb
obulareddyveera <obulareddy.veera@gmail.com>
2020-10-04 16:54:41 +0530

    updated cros origin access

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var http = require('http');
-var debug = require('debug')('api.farmer.accounts.easyfarm.co.in:server');
+var http = require("http");
+var debug = require("debug")("api.farmer.accounts.easyfarm.co.in:server");
 var createError = require("http-errors");
 var express = require("express");
+var cors = require('cors');
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -19,7 +20,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+var allowedOrigins = [
+  "http://localhost:3000",
+  "http://farmer.accounts.easyfarm.co.in/",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 require("./dao/config/connection");
 app.use("/", oAuthRouter);
 app.use("/auth/users", usersRouter);
@@ -39,14 +57,13 @@ app.use(function (err, req, res, next) {
  * Create HTTP server.
  */
 
-var http = require('http');
+var http = require("http");
 var server = http.createServer(app);
-console.log('--== process.env.name ', process.env.name);
+console.log("--== process.env.name ", process.env.name);
 if (process.env.name) {
-  console.log('--== APP is running on UnKnown PORT !!!! ==--');
+  console.log("--== APP is running on UnKnown PORT !!!! ==--");
   server.listen();
 } else {
-  console.log('--== APP is running on 3000 PORT !!!! ==--');
+  console.log("--== APP is running on 3000 PORT !!!! ==--");
   server.listen(3000);
 }
-
