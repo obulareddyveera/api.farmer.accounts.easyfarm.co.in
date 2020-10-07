66ff921f8c378eaf7060ffe8a257553214b7d2df
obulareddyveera <obulareddy.veera@gmail.com>
2020-10-07 11:17:35 +0530

    New Route: /auth/users/session/active

--- routes/oAuth.js
+++ routes/oAuth.js
@@ -7,10 +7,13 @@ const jwt = require("jsonwebtoken");
 const CONFIG = require("./../config");
 const usersController = require("../dao/controllers/users");
 
-router.get("/auth/*", (req, res, next) => {
+router.use("/auth/*", (req, res, next) => {
+  console.log('<<--== /auth/* ==-->>');
   const token = req.headers["x-access-token"];
+  console.log('--== /auth/* ==-->>', token);
   if (!token)
     return res.status(401).send({ auth: false, message: "No token provided." });
+
   jwt.verify(token, CONFIG.JWTsecret, function (err, decoded) {
     if (err)
       return res
@@ -21,17 +24,22 @@ router.get("/auth/*", (req, res, next) => {
     if (new Date().getTime() > expiry_date) {
       res.status(401).send({ auth: false, message: "Session Expired" });
     }
-    req.currentUser = decoded;
+    req.sessionUser = decoded;
     next();
   });
 });
 
 router.get("/", (req, res) => {
   const referer = req.get("referer");
+  let redirectUrl = `${req.protocol}://${req.headers.host}/auth_callback`;
+  if (referer) {
+    redirectUrl = `${req.protocol}://${req.headers.host}/auth_callback/?referer=${referer}`;
+  }
+
   const oauth2Client = new google.auth.OAuth2(
     CONFIG.oauth2Credentials.client_id,
     CONFIG.oauth2Credentials.client_secret,
-    `https://${req.headers.host}/auth_callback/?referer=${referer}`
+    redirectUrl
   );
   const loginLink = oauth2Client.generateAuthUrl({
     access_type: "offline",
@@ -46,11 +54,14 @@ router.get("/", (req, res) => {
 
 router.get("/auth_callback", async (req, res) => {
   const { query } = req;
-  console.log('--=== I am ur referer ', query);
+  let redirectUrl = `${req.protocol}://${req.headers.host}/auth_callback`;
+  if (query.referer) {
+    redirectUrl = `${req.protocol}://${req.headers.host}/auth_callback/?referer=${query.referer}`;
+  }
   const oauth2Client = new google.auth.OAuth2(
     CONFIG.oauth2Credentials.client_id,
     CONFIG.oauth2Credentials.client_secret,
-    `https://${req.headers.host}/auth_callback/?referer=${query.referer}`
+    redirectUrl
   );
   if (query.error) {
     return res.json({ error: query.error });
@@ -71,27 +82,25 @@ router.get("/auth_callback", async (req, res) => {
             currentUser,
           } = await usersController.setGoogleOAuth2User(JSON.parse(data));
           if (err) return res.json({ error: err });
-          console.log(
-            "--== Request HostName ",
-            req.secure,
-            req.hostname,
-            req.originalUrl
-          );
           const jwtToken = jwt.sign(
             { ...tokens, profile: JSON.parse(data) },
             CONFIG.JWTsecret
           );
-          // return res.json({
-          //   jwt: jwt.sign(
-          //     { ...tokens, profile: JSON.parse(data) },
-          //     CONFIG.JWTsecret
-          //   ),
-          //   referer: query,
-          // });
-          if (query.referer.indexOf('localhost') > -1) {
-            return res.redirect(query.referer + `auth_callback?token=${jwtToken}`);
+
+          if (query.referer) {
+            if (query.referer.indexOf("localhost") > -1) {
+              return res.redirect(
+                query.referer + `auth_callback?token=${jwtToken}`
+              );
+            }
+            return res.redirect(
+              query.referer + `auth_callback.html?token=${jwtToken}`
+            );
           }
-          return res.redirect(query.referer + `auth_callback.html?token=${jwtToken}`);
+
+          return res.send({
+            token: jwtToken,
+          });
         });
       })
       .on("error", (err) => {
--- routes/users.js
+++ routes/users.js
@@ -4,19 +4,20 @@ var router = express.Router();
 var usersController = require("../dao/controllers/users");
 /* GET users listing. */
 router.get("/", async (req, res, next) => {
-  const {currentUser} = req;
+  const { currentUser } = req;
   const response = await usersController.fetchAllUsers();
   res.json(response);
 });
 
-router.post("/duplicate/email", async (req, res) => {
-  const inputEmail = req.body.email;
-  if (inputEmail) {
-    const response = await usersController.validateEmail(inputEmail);
-    res.status(200).json(response);
+router.post("/session/active", async (req, res) => {
+  const { sessionUser } = req;
+  const { profile } = sessionUser;
+  console.log("---== sessionUser profile ", profile);
+  if (!profile) {
+    res.status(200).json({ error: "Invalied Session" });
   }
 
-  res.status(200).json({ error: "provide a valid email address" });
+  res.status(200).json(profile);
 });
 
 module.exports = router;
